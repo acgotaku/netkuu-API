02180b3f658ba89148d29c6b05251dcd879a912b
acgotaku <acgotaku311@gmail.com>
2014-03-07 13:22:24 +0800

    add Thread

--- handlers/netkuu.py
+++ handlers/netkuu.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 # vim: set et sw=4 ts=4 sts=4 ff=unix fenc=utf8:
-
+import threading
 import os
 import http.client
 import pdb
@@ -9,6 +9,7 @@ import codecs
 import time
 import json
 import re
+import logging
 import xml.etree.ElementTree as ET
 __ALL__ = ['Film', 'Xml','List', ]
 site_config = {
@@ -83,6 +84,8 @@ class Xml:
         return xml
     def savexml(self,xml,coding):
         data=xml.decode(encoding=coding,errors='replace')
+        time.sleep(5)
+        logging.info("savexml...")
         f=open(self.cache_file,"w")
         f.write(data)
         f.close()
@@ -90,17 +93,17 @@ class Xml:
         if os.path.exists(self.cache_file):
             mtime=os.path.getmtime(self.cache_file)
             now_time=time.time()
-            if now_time-mtime>86400:
-                self.savexml(self.getxml(self.path),self.coding)
-        else:
-            self.savexml(self.getxml(self.path),self.coding)
+            if now_time-mtime<86400:
+                return
+        t = threading.Thread(target=self.savexml, args = (self.getxml(self.path),self.coding))
+        t.daemon = True
+        t.start()
     def getJSON(self,data):
         json_string=""
         if type(data).__name__=="list":
             json_string = json.dumps([item.__dict__ for item in data])
         else:
             json_string = json.dumps(data.__dict__)
-        # pdb.set_trace()
         return json_string
 class Search(Xml):
     def __init__(self,path,cache_file,url="movie.zzti.edu.cn",coding="gb18030"):
@@ -108,7 +111,10 @@ class Search(Xml):
         self.updatexml()
     def readxml(self):
         films=[]
-        f = codecs.open(self.cache_file, mode='r', encoding='utf8')
+        if os.path.exists(self.cache_file):
+            f = codecs.open(self.cache_file, mode='r', encoding='utf8')
+        else:
+            return films
         data=f.read()
         root = ET.fromstring(data)
         f.close()
